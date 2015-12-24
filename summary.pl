b358817dcd074f8d664005d6599378c533fc8f32
Bart Trojanowski <bart@jukie.net>
2015-12-24 11:24:45 -0500

    summary.pl - move set combining into walk

--- summary.pl
+++ summary.pl
@@ -34,6 +34,12 @@ sub expand_nested {
 }
 
 my $full_dump = {
+        # configuration...
+
+        combine_sets => 1,
+
+        # handlers...
+
         fws => sub {
                 my ($s) = @_ ;
         },
@@ -61,70 +67,17 @@ my $full_dump = {
                 my $n = $s->{activity}->{name};
 
                 print "  #$i - $n\n";
-
-                # prepare for walking the sets
-                $s->{set_rept} = 0;
-                $s->{set_last} = "";
         },
         set => sub {
                 my ($s) = @_ ;
-
-                my $this = "";
-
-                # first, convert the set into test
-                if (defined $s->{set}->{weight}) {
-                        my $kg_to_lb = 2.20462;
-                        my $lb = myround ($s->{set}->{weight} * $kg_to_lb);
-
-                        $this = "$lb";
-
-                        my $reps = $s->{set}->{reps};
-                        $this .= " x $reps" if defined $reps;
-
-                } elsif (defined $s->{set}->{duration}) {
-                        my $sec = $s->{set}->{duration} / 1000;
-                        $this = "$sec sec";
-
+                if ($s->{set_rept}>1) {
+                        print "    ", $s->{set_text}, " x ", $s->{set_rept}, "\n";
                 } else {
-                        $this = "BW x " . $s->{set}->{reps};
-                }
-
-                # now figure out if it's a repeat
-                if ($this eq $s->{set_last}) {
-
-                        # same work as last time, increment the repeats
-                        $s->{set_rept} ++;
-
-                } else {
-                        # diffent workload, so dump the current set first
-                        if ($s->{set_rept}>0) {
-                                if ($s->{set_rept}>1) {
-                                        print "    ", $s->{set_last}, " x $s->{set_rept}\n";
-                                } else {
-                                        print "    ", $s->{set_last}, "\n";
-                                }
-                        }
-
-                        # now start a new set
-                        $s->{set_last} = $this;
-                        $s->{set_rept} = 1;
+                        print "    ", $s->{set_text}, "\n";
                 }
         },
         activity_end => sub {
                 my ($s) = @_ ;
-
-                # close last set
-                if ($s->{set_rept}>0) {
-                        if ($s->{set_rept}>1) {
-                                print "    ", $s->{set_last}, " x ", $s->{set_rept}, "\n";
-                        } else {
-                                print "    ", $s->{set_last}, "\n";
-                        }
-                }
-
-                # cleanup
-                undef $s->{set_rept};
-                undef $s->{set_last};
         },
         session_end => sub {
                 my ($s) = @_ ;
@@ -134,6 +87,30 @@ my $full_dump = {
         },
 };
 
+sub format_set {
+        my ($set) = @_;
+
+        my $text;
+
+        if (defined $set->{weight}) {
+                my $kg_to_lb = 2.20462;
+                my $lb = myround ($set->{weight} * $kg_to_lb);
+
+                $text = "$lb";
+
+                my $reps = $set->{reps};
+                $text .= " x $reps" if defined $reps;
+
+        } elsif (defined $set->{duration}) {
+                my $sec = $set->{duration} / 1000;
+                $text = "$sec sec";
+
+        } else {
+                $text = "BW x " . $set->{reps};
+        }
+
+        return $text;
+}
 
 sub walk_fws {
         my ($h, $fws) = @_;
@@ -154,16 +131,61 @@ sub walk_fws {
 
                         $h->{activity}($s) if defined $h->{activity};
 
-                        my $completedSets = $s->{activity}->{performance}->{completedSets};
-                        for ( $s->{set_number}=0; $s->{set_number}<=$#$completedSets; $s->{set_number}++ ) {
+                        if ($h->{combine_sets}) {
 
-                                $s->{set} = $completedSets->[$s->{set_number}];
+                                # prepare for walking the sets
+                                $s->{set_text} = "";
+                                $s->{set_rept} = 0;
 
-                                $h->{set}($s) if defined $h->{set};
+                                my $completedSets = $s->{activity}->{performance}->{completedSets};
+                                for ( $s->{set_number}=0; $s->{set_number}<=$#$completedSets; $s->{set_number}++ ) {
+
+                                        $s->{set} = $completedSets->[$s->{set_number}];
+
+                                        my $text = format_set($s->{set});
+
+                                        if ($text eq $s->{set_text}) {
+
+                                                # same work as last time, increment the repeats
+                                                $s->{set_rept} ++;
+
+                                        } else {
+                                                # diffent workload, so dump the current set first
+                                                if ($s->{set_rept}>0) {
+                                                        $h->{set}($s) if defined $h->{set};
+                                                }
+
+                                                # now start a new set
+                                                $s->{set_text} = $text;
+                                                $s->{set_rept} = 1;
+                                        }
+                                }
+
+                                # the last set was not shown yet
+                                if ($s->{set_rept}>0) {
+                                        $h->{set}($s) if defined $h->{set};
+                                }
+
+                                # cleanup
+                                undef $s->{set_number};
+                                undef $s->{set};
+                        } else {
+                                $s->{set_rept} = 1;
+
+                                my $completedSets = $s->{activity}->{performance}->{completedSets};
+                                for ( $s->{set_number}=0; $s->{set_number}<=$#$completedSets; $s->{set_number}++ ) {
+
+                                        $s->{set} = $completedSets->[$s->{set_number}];
+
+                                        $s->{set_text} = format_set($s->{set});
+
+                                        $h->{set}($s) if defined $h->{set};
+
+                                }
+                                undef $s->{set_number};
+                                undef $s->{set};
 
                         }
-                        undef $s->{set_number};
-                        undef $s->{set};
 
                         $h->{activity_end}($s) if defined $h->{activity_end};
                 }
@@ -197,8 +219,6 @@ sub summarize_file {
         my $fws = $e->{'fws.json'};
 
         walk_fws($full_dump, $fws);
-
-        #summarize_fws($fws);
 }
 
 my $file;
